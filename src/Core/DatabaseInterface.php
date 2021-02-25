fa6a5cc61f429485d8fb5aad85e4ba5052cec4c5
Nicholas English <oxuwazet@gmail.com>
2021-02-25 04:41:43 -0500

    Create DatabaseInterface.php

---
+++ src/Core/DatabaseInterface.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+/**
+ * GNU Affero General Public License v3.0
+ * 
+ * Copyright (c) 2021 Four Way Chess
+ * 
+ * Permissions of this strongest copyleft license are conditioned on making available complete source code
+ * of licensed works and modifications, which include larger works using a licensed work, under the same license.
+ * Copyright and license notices must be preserved. Contributors provide an express grant of patent rights.
+ * When a modified version is used to provide a service over a network, the complete source code of the
+ * modified version must be made available.
+ */
+
+namespace FourWayChess\Core;
+
+/**
+ * A secure by defualt database abstraction.
+ */
+interface DatabaseInterface
+{
+}
